a0b0781d96f90f764efd9b0a12a74a2b9979f7e7
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-06-03 08:33:08 -0400

    update

--- textfade/main.js
+++ textfade/main.js
@@ -5,17 +5,14 @@ const textPhrases = [
   "Have a great day",
 ];
 
-function updateText() {
-  let textElement = document.getElementById('dynamic-text');
-  let currentIndex = 0;
-
+function updateText(){
+  let textElement = document.getElementById('dynamic-text')
+  let currentIndex = 0
   setInterval(function() {
-    textElement.textContent = textPhrases[currentIndex];
-    currentIndex = (currentIndex + 1) % textPhrases.length;
-  }, 3000); // Change text every 5 seconds
+    textElement.textContent = textPhrases[currentIndex]
+    currentIndex = (currentIndex + 1) % textPhrases.length
+  }, 3000)
 }
-
-// Start updating text when the page loads
-window.onload = function() {
-  updateText();
-};
+window.onload = function(){
+  updateText()
+}
