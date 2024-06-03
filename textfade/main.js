b16869076e2350a3f4af0fe616066a5e72201c4d
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-06-03 08:11:19 -0400

    update

--- textfade/main.js
+++ textfade/main.js
@@ -1,21 +1,18 @@
 const textPhrases = [
-  "Welcome to Dynamic Text Animation",
-  "Add a dynamic element to your webpage",
-  "Engage your visitors with dynamic content",
-  "Create visually appealing text effects",
-  "Experiment with different animation techniques"
+  "Simple text animation",
+  "Add additional text as you like",
+  "HTML CSS Javascript only",
+  "Have a great day",
 ];
 
-// Function to loop through text phrases and update text element
 function updateText() {
   let textElement = document.getElementById('dynamic-text');
   let currentIndex = 0;
 
-  // Update text every 5 seconds
   setInterval(function() {
     textElement.textContent = textPhrases[currentIndex];
     currentIndex = (currentIndex + 1) % textPhrases.length;
-  }, 5000); // Change text every 5 seconds
+  }, 3000); // Change text every 5 seconds
 }
 
 // Start updating text when the page loads
