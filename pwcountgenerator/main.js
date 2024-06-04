a48ac2d78a21fde3fa9bdb8853797417c1f659a7
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-06-04 09:32:13 -0400

    linkhover

--- pwcountgenerator/main.js
+++ pwcountgenerator/main.js
@@ -1,15 +1,16 @@
-document.getElementById("generate-btn").addEventListener("click", function() {
-    var length = document.getElementById("length").value;
-    var password = generatePassword(length);
-    document.getElementById("password-display").textContent = password;
-});
 
-function generatePassword(length) {
+document.getElementById('generate-btn').addEventListener('click', function() {
+    var length = document.getElementById('length').value;
+    var password = generatePassword(length)
+    document.getElementById("password-display").textContent = password
+})
+
+function generatePassword(length){
     var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
     var password = "";
-    for (var i = 0; i < length; i++) {
-        var randomIndex = Math.floor(Math.random() * charset.length);
-        password += charset.charAt(randomIndex);
+    for(var i = 0; i < length; i++){
+        var randomIndex = Math.floor(Math.random() * charset.length)
+        password += charset.charAt(randomIndex)
     }
-    return password;
+    return password
 }
